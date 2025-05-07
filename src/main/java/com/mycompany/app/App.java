33095ef59f72ce334053babd1d65adcb42300057
Tharun Kumar S <tharunsaran3662@gmail.com>
2025-05-08 00:59:05 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "i love u tharunkumar!";
+    private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
